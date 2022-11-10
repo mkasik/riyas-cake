60cd6134761d74ad0d81fca43cca30f580c9a5b3
Mk Asik <mkasik123@gmail.com>
2022-11-10 14:37:47 +0600

    added blog section

--- src/HappyCustomer/HappyCustomer.js
+++ src/HappyCustomer/HappyCustomer.js
@@ -10,18 +10,18 @@ const HappyCustomer = () => {
             <div class="grid grid-cols-3 gap-4 mt-6 mb-8">
                 <div  >
                     <img className='h-40 w-40 m-auto rounded-full  ' alt='' src={img1}></img>
-                    <p className='m-auto text-base mt-2 '> “Amazing food. PERIOD. Service is always perfect, food is always on point, and the staff is very friendly and accommodating. Always looking forward to this place with my friends, the fried vanilla ice cream is exceptionally good!“</p>
+                    <p className='m-auto text-base mt-2 w-2/3'> “Amazing food. PERIOD. Service is always perfect, food is always on point and your all cake are attrack to me so good “</p>
                     <h3 className='text-2xl font-semibold mt-1'>David Max</h3>
 
                 </div>
                 <div class="">
                     <img className='h-40 m-auto w-40 rounded-full' alt='' src={img2}></img>
-                    <p className='m-auto text-base mt-2'> “Service is always perfect, food is always on point, Amazing food. PERIOD.  and the staff is very friendly and accommodating. Always looking forward to this place with my friends, the fried vanilla ice cream is exceptionally good!“</p>
+                    <p className='m-auto text-base mt-2 w-2/3'> “Service is always perfect, food is always on point, Amazing food. PERIOD.  and the cake is very testy and “</p>
                     <h3 className='text-2xl font-semibold mt-1'>Palisa </h3>
                 </div>
                 <div class="...">
                     <img className='h-40 m-auto w-40 rounded-full' alt='' src={img3}></img>
-                    <p className='m-auto text-base mt-2'> Service is always perfect, food is always on point, Amazing food. PERIOD.  and the staff is very friendly and accommodating. Always looking forward to this place with my friends, the fried vanilla ice cream is exceptionally good!“ </p>
+                    <p className='m-auto text-base mt-2 w-2/3'>  Food is always on point, Amazing food. PERIOD.  and delivery is very fast and I am glad. </p>
                     <h3 className='text-2xl font-semibold mt-1'>Jon Son</h3>
                 </div>
 
--- src/Pages/Blog/Blog/Blog.js
+++ src/Pages/Blog/Blog/Blog.js
@@ -3,7 +3,134 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>This is blog</h2>
+            <h2 className='text-2xl font-semibold mt-6 mb-6'>Blog</h2>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item bg-white border border-gray-200">
+                    <h2 className="accordion-header mb-0" id="headingOne">
+                        <button className="
+        accordion-button
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                            aria-controls="collapseOne">
+                            <strong> What is the difference between SQL and NoSQL?
+                            </strong>                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample">
+                        <div className="accordion-body py-4 px-5">
+                            SQL databases are vertically scalable, while NoSQL databases are horizontally scalable. SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores. SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item bg-white border border-gray-200">
+                    <h2 className="accordion-header mb-0" id="headingOne">
+                        <button className="
+        accordion-button
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true"
+                            aria-controls="collapseOne">
+                            <strong> How does NodeJS handle multiple requests at the same time?
+                            </strong>                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample">
+                        <div className="accordion-body py-4 px-5">
+                            I understand that Node.js uses a single-thread and an event loop to process requests only processing one at a time (which is non-blocking). But still, how does that work, lets say 10,000 concurrent requests. The event loop will process all the requests? Would not that take too long?
+
+                            I can not understand (yet) how it can be faster than a multi-threaded web server. I understand that multi-threaded web server will be more expensive in resources (memory, CPU), but would not it still be faster? I am probably wrong; please explain how this single-thread is faster in lots of requests, and what it typically does (in high level) when servicing lots of requests like 10,000.
+
+                            And also, will that single-thread scale well with that large amount? Please bear in mind that I am just starting to learn Node.js.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item bg-white border border-gray-200">
+                    <h2 className="accordion-header mb-0" id="headingTwo">
+                        <button className="
+        accordion-button
+        collapsed
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                            aria-controls="collapseTwo">
+                            <strong> What is JWT, and how does it work?
+                            </strong>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo"
+                        data-bs-parent="#accordionExample">
+                        <div className="accordion-body py-4 px-5">
+                            JWT, or JSON Web Token, is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.
+
+                            JWT differ from other web tokens in that they contain a set of claims. Claims are used to transmit information between two parties. What these claims are depends on the use case at hand. For example, a claim may assert who issued the token, how long it is valid for, or what permissions the client has been granted
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item bg-white border border-gray-200">
+                    <h2 className="accordion-header mb-0" id="headingThree">
+                        <button className="
+        accordion-button
+        collapsed
+        relative
+        flex
+        items-center
+        w-full
+        py-4
+        px-5
+        text-base text-gray-800 text-left
+        bg-white
+        border-0
+        rounded-none
+        transition
+        focus:outline-none
+      " type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
+                            aria-controls="collapseThree">
+                            <strong>What is the difference between javascript and NodeJS?</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree"
+                        data-bs-parent="#accordionExample">
+                        <div className="accordion-body py-4 px-5">
+                            <strong> 1. NodeJS :</strong>
+                            NodeJS is a cross-platform and opensource Javascript runtime environment that allows the javascript to be run on the server-side. Nodejs allows Javascript code to run outside the browser. Nodejs comes with a lot of modules and mostly used in web development.<br></br>
+                            <strong> 2. JavaScript : </strong>
+
+                            Javascript is a Scripting language. It is mostly abbreviated as JS. It can be said that Javascript is the updated version of the ECMA script. Javascript is a high-level programming language that uses the concept of Oops but it is based on prototype inheritance.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
